078529863e6c8327699acac05905e0b9b47862b4
dssd <gonza.edelp77@gmail.com>
2018-05-10 18:58:29 -0300

    get credentials dynamodb from config vars

--- db.js
+++ db.js
@@ -12,8 +12,6 @@ console.log("--------Clave",process.env.AWS_ACCESS_KEY_ID)
 /*var AWS = require('aws-sdk');
 var config = {
   "apiVersion": "2012-08-10",
-  "accessKeyId": "abcde",
-  "secretAccessKey": "abcde",
   "region":"us-west-2",
   "endpoint": "http://localhost:8000"
 }
