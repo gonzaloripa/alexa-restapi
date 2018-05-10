31c0818b99c9222ebbec904d7c752b6de7fc148a
dssd <gonza.edelp77@gmail.com>
2018-05-10 17:48:58 -0300

    aver

--- db.js
+++ db.js
@@ -2,8 +2,8 @@
 var dynamoose = require('dynamoose');
 //dynamoose.local();
 dynamoose.AWS.config.update({
-  accessKeyId: 'AKIAI3NDSBREHM57OXBQ',//process.env.AWS_ACCESS_KEY_ID,//'AKIAI3NDSBREHM57OXBQ',
-  secretAccessKey: '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',//process.env.AWS_SECRET_ACCESS_KEY,//'BAELD5ty1LCvuHWIETfGEyLPimmIH7jXV/S4vC3F',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,//'AKIAI3NDSBREHM57OXBQ',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,//'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',
   region: 'us-east-1'
 
 });
