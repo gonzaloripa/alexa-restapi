54d547255b460302dbfac3e6c9872dbbeaeba29b
dssd <gonza.edelp77@gmail.com>
2018-05-21 00:59:52 -0300

    Configurando mongoose

--- db.js
+++ db.js
@@ -1,18 +1,10 @@
 // db.js:conectamos la base con el servidor
-var dynamoose = require('dynamoose');
-//dynamoose.local();
-dynamoose.AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'us-east-1'
 
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
 });
-
-console.log("--------Clave",process.env.AWS_ACCESS_KEY_ID)
-/*var AWS = require('aws-sdk');
-var config = {
-  "apiVersion": "2012-08-10",
-  "region":"us-west-2",
-  "endpoint": "http://localhost:8000"
-}
-var dynamodb = new AWS.DynamoDB(config);*/
