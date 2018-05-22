b9f0dfc4296a7364194c6a04f1542783a3a76460
dssd <gonza.edelp77@gmail.com>
2018-05-22 19:33:08 -0300

    Mod db-uri

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 // db.js:conectamos la base con el servidor
 
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI_BLACK, function (err, res) {
+mongoose.connect(process.env.MONGOLAB_BLACK_URI, function (err, res) {
   if (err) { 
-    console.log ('ERROR connecting to: ' + process.env.MONGODB_URI_BLACK + '. ' + err);
+    console.log ('ERROR connecting to: ' + process.env.MONGOLAB_BLACK_URI + '. ' + err);
   } else {
-    console.log ('Succeeded connected to: ' + process.env.MONGODB_URI_BLACK);
+    console.log ('Succeeded connected to: ' + process.env.MONGOLAB_BLACK_URI);
   }
 });
 
