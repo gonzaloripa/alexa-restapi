a531a6437968089a340e1f9d6d0722a1fab1c960
dssd <gonza.edelp77@gmail.com>
2018-05-23 01:18:37 -0300

    put operation added

--- user/UserController.js
+++ user/UserController.js
@@ -48,60 +48,36 @@ router.get('/:usrid/:name', function (req, res) {
     });
 });
 
-/*
-
-//Modificar una noticia de un usuario 
-router.put('/update/user/:name',function(req, res) {
-	//req.body.xpath:body/div[1]/div[1]/div[1]/div[2]/section[1]/article[1]/a[1]/h2[1]
-	//req.body.url: 'https://diariohoy.net'
-	console.log(req.params.name)
-	var userid;
-	User.scan('name').contains(req.params.name).exec(function (err, usuarios) {
-	  
-	  userid=usuarios[0].userid;
-	  var array = usuarios[0].noticias;
-	  var encontro;
-	  array.forEach(function(elem){
-	  	if( JSON.stringify(req.body) != JSON.stringify(elem)){	  	
-	  	  encontro = true;
-	  	}
-	  });
-	  if(encontro){ //Si la noticia es distinta a las que ya tenia el usuario
-		  array.push(req.body);
-		  console.log('array ' + array,usuarios[0].noticias);
-		  User.update({userid: userid, name: req.params.name}, {noticias: array}, function (err,user) {//{url:req.body.url,xpath:req.body.xpath}
-			if(err) { return console.log(err); }
-			console.log('Actualizado ',req.body);
-			
-			res.send(user);
-		  })
-	  }else{
-		res.send("Ya existe la noticia para ese usuario");	
-	  }
-	});
-	User.get('amzn1.ask.account.AEM7C7O3S3FKO4J77F7YYBP5CXPUVG4VHEW4MM77YUETWFCQAMJE4PTXRJCZAJTWC2FKIP3MEVBILLNA2TK7VDHVBHBDA7ZSFLFRYWYE2U4WBV64CWFAKL74DHSBJ3KHY2VPD6HY7G5AWN5XUUIQCJYOQ3VAMD32MKA63PW5ZEDG5F2AXOIL5VNSGPKZZDY3IFDK4V75RD4CKYY','gonza').then(function (usuario) {
-	  //console.log('Usuario ' + usuario.userid);
-	  userid=usuario.userid;
-	})
-  });
-
-*/
-
-
-/*
 // DELETES A USER FROM THE DATABASE
-router.delete('/:usrid', function (req, res) {
-    User.delete({userid: req.params.usrid, name: 'gonza'}, function(err) {
-	  if(err) { return console.log(err); }
-	  console.log('Usuario eliminado ',req.params.usrid);
-	  res.status(200).send('Usuario eliminado')
-	});
-    /*User.findByIdAndRemove(req.params.usrid, function (err, user) {
+router.delete('/:usrid/:name', function (req, res) {
+    User.findOneAndDelete({"userId":req.params.usrid,"name":req.params.name}, function (err, user) {
         if (err) return res.status(500).send("There was a problem deleting the user.");
         res.status(200).send("User "+ user.name +" was deleted.");
     });
-    
 });
 
-*/
+//UPDATE A USER WITH A NEW NOTICE 
+router.put('/update/user/:name',function(req, res) {
+	//req.body.xpath:body/div[1]/div[1]/div[1]/div[2]/section[1]/article[1]/a[1]/h2[1]
+	//req.body.url: 'https://diariohoy.net'
+	
+	var userid;
+	var array;
+	//Obtengo solo los campos userId y noticias del usuario con nombre :name
+	User.find({ name: req.params.name }, 'userId, noticias', function (err, docs) { 
+		console.log("-----userId and noticias : "+docs);
+		userid = docs.userId;
+		array = docs.noticias;
+		array.push(req.body);//Agrego una nueva noticia a las que ya tenia el usuario
+		
+		var query = { userId: userid, name: req.params.name };
+		User.findOneAndUpdate(query, { $set: { noticias: array }}, function (err,user) {//{url:req.body.url,xpath:req.body.xpath}
+				if(err) return res.status(500).send("There was a problem updating the user.");
+				console.log('Actualizado ',req.body);
+				
+				res.status(200).send(user);
+		});
+	})
+});
+
 module.exports = router;
