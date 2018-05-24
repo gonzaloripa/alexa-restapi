fb4d0f0d96a66b37df3a379cbe8f60a7d1d0a832
dssd <gonza.edelp77@gmail.com>
2018-05-24 17:34:51 -0300

    get notice

--- user/UserController.js
+++ user/UserController.js
@@ -41,11 +41,11 @@ router.get('/', function (req, res) {
 
 // GETS A SINGLE USER FROM THE DATABASE
 router.get('/:usrid/:name', function (req, res) {
-    User.find({'userId':req.params.usrid,'name':req.params.name}, function (err, user) {
+    User.find({'userId':req.params.usrid,'name':req.params.name},{ '_id': 0, 'name' :1}, function (err, name) {
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
-        console.log(user[0];
-        res.status(200).send(user[0].name);
+        console.log(name);
+        res.status(200).send(name);
     });
 });
 
