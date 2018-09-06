569bf61852d54d0d85397f17e056aeb78ddaaa64
dssd <gonza.edelp77@gmail.com>
2018-09-06 20:34:17 -0300

    aver

--- user/UserController.js
+++ user/UserController.js
@@ -126,7 +126,7 @@ router.put('/addListContent/user/:name',function(req, res) {
 	//req.body.url: 'https://diariohoy.net'
   //req.body.state = 'new'/'old'
 	var contBody = req.body;
-  var query = { 'name': req.params.name.toLowerCase(),'contenidos._id': {$ne: contBody._id}} };//agregar password
+  var query = { 'name': req.params.name.toLowerCase(),'contenidos.url': {$ne: contBody.url},'contenidos.xpath':{$ne:contBody.xpath} };//agregar password
   User.findOneAndUpdate(query, { $push: { contenidos:{$each: contBody} }}, function (err,user) {//{url:req.body.url,xpath:req.body.xpath}
     if(err) return res.status(500).send("There was a problem updating the user.");
     //user contiene el usuario antes de ser actualizado
