0d23800c4f3073ea631a4c51b52ccc2647fb848b
dssd <gonza.edelp77@gmail.com>
2018-09-06 23:00:10 -0300

    aver

--- user/UserController.js
+++ user/UserController.js
@@ -76,10 +76,10 @@ router.get('/noticesByState/:state/:name', function (req, res) {
 
 var getCriteria = {'name':req.params.name.toLowerCase(),'contenidos.state':req.params.state};
 
-User.find(getCriteria,{ '_id': 0,'contenidos.$' : 1},function(err, result){
+User.find(getCriteria,{ '_id': 0,'contenidos.$' : 2},function(err, result){
     if (err) return res.status(500).send("There was a problem finding the user.");
       if (!result || result.length == 0) return res.status(404).send("No user found.");
-      console.log(result)
+      console.log(result[0].contenidos)
       res.status(200).send(result[0].contenidos);
   });
   
