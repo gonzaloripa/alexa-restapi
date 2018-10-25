a7e64e73e9953adda51d8a2aed04ce775fe63781
dssd <gonza.edelp77@gmail.com>
2018-10-25 00:53:07 -0300

    get contents by order

--- user/UserController.js
+++ user/UserController.js
@@ -105,8 +105,10 @@ router.get('/noticesByOrder/:name', function (req, res) {
     var getCriteria = {'name':req.params.name.toLowerCase()}//,'contenidos.state':req.params.state};
     User.aggregate(
        [
+        {$unwind: "$contenidos"},
         { $match: getCriteria },
         { $sort : { "contenidos.order": 1}}
+        {$group: {_id:"$_id", contents: {$push:"$contenidos"}}}
        ])
     .then(function (result) {
       console.log(result); // [ { maxBalance: 98000 } ]
