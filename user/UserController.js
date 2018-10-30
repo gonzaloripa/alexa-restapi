56d34b92818b6b65e2dfbec3bb19db439a826ac2
dssd <gonza.edelp77@gmail.com>
2018-10-30 16:49:41 -0300

    agrego prueba

--- user/UserController.js
+++ user/UserController.js
@@ -44,7 +44,7 @@ router.get('/prueba',function(req,res){
           var getElementByXpath = function(path) {
               //console.log("-------Path en getElement: ",xpath.select(path,docu)[0].nodeValue);
               //console.log("-------Evaluate: ",xpath.evaluate(path, docu, null, xpath.XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.lastChild.data);
-              return (xpath.evaluate(path, dom.window.document, null, xpath.XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue);
+              return (xpath.evaluate(path, dom, null, xpath.XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue);
           }
           //console.log("----Element a ",findElementA(getElementByXpath("//"+noticia.xpath)).attributes[1].nodeValue)
           
