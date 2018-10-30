9624dd5a4f7a38ae2a4a52bdbeb9abbe5bb84707
dssd <gonza.edelp77@gmail.com>
2018-10-30 15:41:29 -0300

    agrego prueba

--- user/UserController.js
+++ user/UserController.js
@@ -29,7 +29,7 @@ router.get('/prueba',function(req,res){
       })
       .then(body => {
           //console.log("---Body: ",body)    
-          var docu = new DOMParser().parseFromString(body,'text/html')
+          var docu = new dom().parseFromString(body,'text/xml')
           //console.log("---Body: ",docu)    
           var getElementByXpath = function(path) {
               //console.log("-------Path en getElement: ",xpath.select(path,docu)[0].nodeValue);
