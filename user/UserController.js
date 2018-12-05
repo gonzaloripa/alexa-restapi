a34a845d586c347aaf2abadd6d46b313c33e2532
dssd <gonza.edelp77@gmail.com>
2018-12-04 22:59:33 -0300

    aver

--- user/UserController.js
+++ user/UserController.js
@@ -262,7 +262,7 @@ router.put('/updateListContents/user/:name', function (req, res) {
                    })
             .exec((err, result)=> {
               console.log("result "+result)
-              if (result.length == 0){ //No existe el idConjunto
+              if (result.contenidos.length == 0){ //No existe el idConjunto
                 var updates = req.body.map((item,index)=>{
                     console.log("item "+item.url+item.xpath+index)
                     return User.update({'name':req.params.name.toLowerCase()}, 
