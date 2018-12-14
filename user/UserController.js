6edc8a039a205a6bfc59ab39e1e30a993be40679
dssd <gonza.edelp77@gmail.com>
2018-12-14 12:48:51 -0300

    tests

--- user/UserController.js
+++ user/UserController.js
@@ -146,7 +146,7 @@ router.get('/noticesByOrder/:flow/:name', function (req, res) {
         //flows será un [] de flow_id
         console.log("Flujos: ",flows,userId)
         Model.Content.find({
-          'contents.user_id': userId,
+          'user_id': userId,
           'contents.flow_id': flows[0]._id
         })
         /*Model.Content.aggregate(
