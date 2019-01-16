c7e7adb93b7532d253ee8c81e2f5c42ba247b104
dssd <gonza.edelp77@gmail.com>
2019-01-16 17:19:53 -0300

    add endpotin

--- user/UserController.js
+++ user/UserController.js
@@ -457,7 +457,7 @@ router.get('/contentsByOrder/:flow/:name', function (req, res) {
             {
               $project:{
                 infoContents:1,
-                combinedC:1,
+                //combinedC:1,
                 _id:0
               }
             },
