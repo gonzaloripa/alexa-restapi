6a9f7d3d3550b339e99a377ee544ad81f23de7f4
dssd <gonza.edelp77@gmail.com>
2019-01-16 17:55:14 -0300

    add endpotin

--- user/UserController.js
+++ user/UserController.js
@@ -446,24 +446,24 @@ router.get('/contentsByOrder/:flow/:name', function (req, res) {
                  }
                }
             },
-            { $unwind: '$combinedC'},/*
+            { $unwind: '$combinedC'},
             { $lookup: {
                 from: 'infocontents',
                 localField: 'combinedC',
                 foreignField: '_id',
                 as: 'infocontents'
               }
-            },*/
+            },
             {
               $project:{
-                //infoContents:1,
-                combinedC:1,
+                infoContents:1,
+                //combinedC:1,
                 _id:0
               }
-            },
+            }/*,
             { 
               $sort: {'cont.order': 1 }
-            }
+            }*/
            ])
           .exec(function (err,result) {
               console.log("-Contents id %s ",result)
@@ -526,18 +526,18 @@ router.get('/contentsByCategory/:category/:name', function (req, res) {
                  }
                }
             },
-            { $unwind: '$combinedC'},/*
+            { $unwind: '$combinedC'},
             { $lookup: {
                 from: 'infocontents',
                 localField: 'combinedC',
                 foreignField: '_id',
                 as: 'infocontents'
               }
-            },*/
+            },
             {
               $project:{
-                //infocontents:1,
-                combinedC:1,
+                infocontents:1,
+                //combinedC:1,
                 _id:0
               }
             }
