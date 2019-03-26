390e1766164227c8fd7f98063ce1b2f251a57433
dssd <gonza.edelp77@gmail.com>
2019-03-26 16:24:38 -0300

    added get data info

--- user/UserController.js
+++ user/UserController.js
@@ -676,7 +676,7 @@ router.get('/contentsByOrder/:flow/:name', function (req, res) {
             },
             { $lookup: {
                 from: 'infocontents',
-                localField: '$combinedC',
+                localField: 'combinedC',
                 foreignField: '_id',
                 as: 'info'
               }
