6af1bfa8592482200d419d45e6df55a0a058e558
dssd <gonza.edelp77@gmail.com>
2019-03-26 17:54:39 -0300

    added get data info

--- user/UserController.js
+++ user/UserController.js
@@ -687,9 +687,11 @@ router.get('/contentsByOrder/:flow/:name', function (req, res) {
               $group:{
                 _id:'$_id',
                 content:{
-                  idcontent:'$dataContent._id',
-                  infocontent:'$dataContent',
-                  data:'$combinedC.data'
+                  $push:{
+                    idcontent:'$dataContent._id',
+                    infocontent:'$dataContent',
+                    data:'$combinedC.data'
+                  }
                 }
               }
             },
