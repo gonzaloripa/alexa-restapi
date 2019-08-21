c67a260a28ad57443ad87976ff6f6f1b8ede47ae
dssd <gonza.edelp77@gmail.com>
2019-08-21 16:11:41 -0300

    delete content in cascade

--- user/UserController.js
+++ user/UserController.js
@@ -664,7 +664,7 @@ router.put('/setContentUnavailable/:name',function(req, res) {
 });
 
 router.delete('/deleteContentUnavailable/:name',function(req,res){
-
+      console.log("Entra en deleteContentUnavailable")
       var content = req.body
       Model.User.findOne({'name':req.params.name.toLowerCase()},'_id',
         function(err,userId){
