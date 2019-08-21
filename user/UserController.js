430b28a2c7af1ee147b85bd584565bdc3c16ee20
dssd <gonza.edelp77@gmail.com>
2019-08-21 19:07:46 -0300

    delete content in cascade

--- user/UserController.js
+++ user/UserController.js
@@ -673,8 +673,8 @@ router.delete('/deleteContentUnavailable/:name',function(req,res){
             function(err,content){
                 console.log("--content delete ",content, content.deletedCount)
 
-                Model.Flow.update( { user:userId }, // contents: { $elemMatch: {_id: content._id} }}, 
-                { $pull: {"contents": {_id:content._id } } },
+                Model.Flow.findOneAndUpdate({ user:userId , contents: { $elemMatch: {_id: content._id} }}
+                ,{ $pull: {"contents": {_id:content._id } } },
                 function(err, result) {
                     console.log("Flow update - ",result)
                     if (err) return res.status(500).send("No se pudo eliminar el contenido");
