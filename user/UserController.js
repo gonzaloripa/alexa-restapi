60e055b74e91a8b9837e0abb689039db00d0c233
dssd <gonza.edelp77@gmail.com>
2019-08-23 15:48:19 -0300

    fix endpoint to delete content from infocontent collection

--- user/UserController.js
+++ user/UserController.js
@@ -665,7 +665,7 @@ router.put('/setContentUnavailable/:name',function(req, res) {
 
 router.delete('/deleteContentUnavailable/:name',function(req,res){
       console.log("Entra en deleteContentUnavailable")
-      var content = req.body
+      var content = req.body.content
       Model.User.findOne({'name':req.params.name.toLowerCase()},'_id',
         function(err,userId){
             console.log(userId,content)
