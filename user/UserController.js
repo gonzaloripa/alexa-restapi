486bd7c5a1ff790c34d5e309e0fff7835429fefd
dssd <gonza.edelp77@gmail.com>
2019-09-26 20:04:49 -0300

    getUser fixed

--- user/UserController.js
+++ user/UserController.js
@@ -156,7 +156,7 @@ router.get('/', function (req, res) {
 // RETURNS A USER FILTER BY NAME
 router.get('/:name', function (req, res) {
     Model.User.find({ name:req.params.name.toLowerCase()}, function (err, user) {
-        if (err) return res.status(404).send("No se hallo ningún usuario con ese nombre");
+        if (err || !user) return res.status(404).send("No se hallo ningún usuario con ese nombre");
         res.status(200).send(user[0].name);
     });
 });
